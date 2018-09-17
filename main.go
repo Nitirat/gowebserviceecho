b7d1ca2ba7083186b76078925f739ff40628366f
VERBACK-TWO\VerBack-Aop <nitirat@vertice-international.com>
2018-09-17 10:17:42 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type (
+	user struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+)
+
+var (
+	users = map[int]*user{}
+	seq   = 1
+)
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/users/:id", getUser)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
